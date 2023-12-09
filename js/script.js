7d0b24b3aa27dc8f007f168bbbc8dc9f076efc4d
avostokova <131677243+avostokova@users.noreply.github.com>
2023-12-10 00:36:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* var particles = Particles.init({
+	selector: '.background',
+  color: '#64121d',
+  opacity: 0.7,
+});
+
+ */
+
+VANTA.BIRDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0xffffff,
+  quantity: 3.00,
+  backgroundAlpha: 0.21
+})
